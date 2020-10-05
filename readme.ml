0bfe1437f6e979fe8f284e3303f415b87c41e67f
nmundie <59545869+nmundie@users.noreply.github.com>
2020-10-04 22:31:49 -0400

    Add files via upload

---
+++ readme.ml
@@ -0,0 +1,5 @@
+To run the BabySitter Day Wage Calculation:
+
+1) Install Maven from https://maven.apache.org
+2) To build and Run Junit tests, execute 'mvn clean install'
+3) To execute the main method in the BabySitterHourlyEmployee class, use command mvn exec:java -Dexec.mainClass="com.oh.babysitter.wagecalc.BabySitterHourlyEmployee"
